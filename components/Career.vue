cdacdfc1a39749758e7ae25d659c58d2ae224ff9
faruuhan <farhnn.r@gmail.com>
2023-09-09 15:57:24 +0700

    Update color

--- components/Career.vue
+++ components/Career.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="p-4 bg-white drop-shadow-xl aspect-square rounded-2xl">
-    <h3 class="mb-2 font-cursive text-rose-500">History Career</h3>
+    <h3 class="mb-2 text-pink-600 font-cursive">History Career</h3>
     <div class="flex items-center gap-2 px-2 border-l-4 border-emerald-500">
       <!-- <div>
           <img src="~/static/BGN.jpeg" class="rounded-full w-14" alt="" />
         </div> -->
       <div>
         <h4 class="font-semibold">Software Engineer</h4>
-        <p class="text-sm text-gray-500">PT Biznet Gio Nusantara • Jakarta</p>
-        <p class="text-sm text-gray-500">Nov 22 - Present</p>
+        <p class="text-sm text-gray-600">PT Biznet Gio Nusantara • Jakarta</p>
+        <p class="text-sm text-gray-600">Nov 22 - Present</p>
       </div>
     </div>
   </div>
--- pages/support.vue
+++ pages/support.vue
@@ -2,10 +2,10 @@
   <div class="flex items-center justify-center min-h-screen bg-white-50">
     <div class="px-4">
       <h4 class="text-4xl text-center font-cursive text-zinc-700">Sorry!</h4>
-      <p class="font-sans text-center text-gray-500">
+      <p class="font-sans text-center text-gray-700">
         Your device isn't support, because your device screen less than 1024px.
         Please access from desktop or tablet.
-        <NuxtLink to="/" class="font-sans text-blue-600 hover:text-blue-500"
+        <NuxtLink to="/" class="font-sans text-blue-700 hover:text-blue-500"
           >back to homepage</NuxtLink
         >
       </p>
