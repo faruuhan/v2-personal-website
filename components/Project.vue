f110057947f3955ea4c4c7a1b022edee9f84a0d3
faruuhan <farhnn.r@gmail.com>
2023-09-09 10:56:11 +0700

    update Project.vue

--- components/Project.vue
+++ components/Project.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="relative overflow-hidden bg-white aspect-square rounded-2xl">
-    <nuxt-img src="/project/bringeee.png" class="" alt="" />
+    <nuxt-img src="/project/bringeee.png" width="500" />
     <div
       class="absolute bottom-0 left-0 flex flex-col justify-end w-full h-full p-4 bg-zinc-800/10"
     >
